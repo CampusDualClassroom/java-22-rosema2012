d52628dedc1e7f6c42694fb32241cf99b9a4707e
Happy Days!!! <36199346+rosema2012@users.noreply.github.com>
2024-09-27 10:27:40 +0200

    Ejercicio 22 terminado

--- src/main/java/com/campusdual/classroom/Exercise22.java
+++ src/main/java/com/campusdual/classroom/Exercise22.java
@@ -7,20 +7,42 @@ import java.util.Set;
 public class Exercise22 {
 
     public static List<String> createArrayList() {
-
+        List<String> lista = new ArrayList<>();
+        lista.add("GKFFD");
+        lista.add("TNANA");
+        lista.add("MPMSL");
+        lista.add("PSWME");
+        lista.add("LZMLF");
+        lista.add("JYEBV");
+        lista.add("YELNT");
+        lista.add("JSNKR");
+        lista.add("JFESF");
+        lista.add("TMJLL");
+        return lista;
     }
 
     public static void printElementsAndIndex(List<String> customList) {
-
+        for (int i = 0; i < customList.size(); i++) {
+            System.out.println(customList.get(i));
+        }
     }
 
     public static boolean addElementToList(List<String> customList, String element) {
-
+        int tamAntiguo = customList.size();
+        customList.add(element);
+        int tamNuevo = customList.size();
+        if(tamAntiguo<tamNuevo){
+            return true;
+        }else{
+            return false;
+        }
     }
 
     public static void main(String[] args) {
+        List<String> coleccion = createArrayList();
+        printElementsAndIndex(coleccion);
+
+        addElementToList(coleccion,"AAAAA");
 
     }
-
-
 }
